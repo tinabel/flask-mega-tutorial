d51100a23ab2941a8e80aeedfd7a70aa54878458
Tina Bell Vance <tina@bellvance.com>
2023-06-22 17:34:01 -0400

    Add show_pagination variable for posts

--- app/routes.py
+++ app/routes.py
@@ -40,11 +40,14 @@ def index():
     db.session.commit()
     flash(_('Your post is now live!'))
     return redirect(url_for('index'))
+
   page = request.args.get('page', 1, type=int)
-  posts = current_user.followed_posts().paginate(page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+  posts = current_user.followed_posts().paginate(
+    page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+  show_pagination = posts.total > posts.per_page
   next_url = url_for('index', page=posts.next_num) if posts.has_next else None
   prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
-  return render_template('index.html', form=form, posts=posts, title=_('Home'), next_url=next_url, prev_url=prev_url)
+  return render_template('index.html', form=form, posts=posts.items, title=_('Home'), show_pagination=show_pagination, next_url=next_url, prev_url=prev_url)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
