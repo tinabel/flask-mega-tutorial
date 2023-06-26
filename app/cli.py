ffcd5760a1806eefbe2d14e88bb2245eb928781c
Tina Bell Vance <tina@bellvance.com>
2023-06-26 15:19:17 -0400

    Fix translate methods in cli

--- app/cli.py
+++ app/cli.py
@@ -1,31 +1,35 @@
 import os
 import click
 
+
 def register(app):
     @app.cli.group()
     def translate():
-      # Translation and localization commands.
-      pass
+        """Translation and localization commands."""
+        pass
 
     @translate.command()
     @click.argument('lang')
     def init(lang):
-      # Initializes a new language.
-      if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
-        raise RuntimeError('init command failed')
-      os.remove('messages.pot')
-
-      @translate.command()
-      def update():
-        # Updates all languages.
+        """Initialize a new language."""
         if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
-          raise RuntimeError('extract command failed')
-        if os.system('pybabel update -i messages.pot -d app/translations'):
-          raise RuntimeError('update command failed')
+            raise RuntimeError('extract command failed')
+        if os.system(
+                'pybabel init -i messages.pot -d app/translations -l ' + lang):
+            raise RuntimeError('init command failed')
         os.remove('messages.pot')
 
-      @translate.command()
-      def compile():
-        # Compiles all languages.
+    @translate.command()
+    def update():
+        """Update all languages."""
+        if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+            raise RuntimeError('extract command failed')
+        if os.system('pybabel update -i messages.pot -d app/translations'):
+            raise RuntimeError('update command failed')
+        os.remove('messages.pot')
+
+    @translate.command()
+    def compile():
+        """Compile all languages."""
         if os.system('pybabel compile -d app/translations'):
-          raise RuntimeError('compile command failed')
+            raise RuntimeError('compile command failed')
